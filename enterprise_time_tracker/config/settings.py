be06043de27285c32a5e12e861816348ea7c2e9e
Aakash Bambhaniya <147966604+1118A@users.noreply.github.com>
2026-03-02 18:03:36 +0530

    settings.py

--- enterprise_time_tracker/config/settings.py
+++ enterprise_time_tracker/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gq$1di$rv61t=eb=uhomobh)$t#6hx4q4!9-jz743et*(*t#c9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sky-time-tacker.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
